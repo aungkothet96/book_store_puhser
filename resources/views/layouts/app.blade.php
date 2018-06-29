976fff1a9eabc1eb5763996349d86f1c8185c7d6
Aung Ko Thet <aungko@innov8te.com.sg>
2018-06-29 22:18:34 +0630

    akt-fav icon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> -->
     <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css">
-    
-
+    <!-- Icon -->
+    <link rel="icon" href="{{URL::to('favicon.png')}}" type="image/ico">
     <!-- FontAwsome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <!-- Styles -->
